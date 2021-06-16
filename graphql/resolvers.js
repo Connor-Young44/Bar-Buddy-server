b5a0c9c61723d955a917b4a0c05de23d3193325c
TheCrazedLime <thecrazedlime@gmail.com>
2021-06-16 13:53:44 +0200

    add validations for creating account

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -6,9 +6,12 @@ const { saltRounds } = require("../config/constants");
 //db => models from sequelize
 module.exports = {
   Query: {
+    //all users
     users: async (parent, _args, { db }, info) => {
       return db.user.findAll();
     },
+    //one user
+    //singleUser: async (parent, _args, { db }, info) => {},
     bars: async (parent, _args, { db }, info) => {
       return db.bar.findAll({ include: [db.user, db.table] });
     },
@@ -43,6 +46,16 @@ module.exports = {
       { db },
       info
     ) => {
+      //validate inputs
+      if (!firstName) return new ApolloError("Please Provide First Name", 400);
+      if (!lastName) return new ApolloError("Please Provide Second Name", 400);
+      if (!email) return new ApolloError("Please Provide email", 400);
+      if (!password) return new ApolloError("Please create a password", 400);
+      //check for unique email
+      const user = await db.user.findOne({ where: { email } });
+      //console.log(user);
+      if (user) return new ApolloError("Only 1 user is allowed that email");
+      //if all inputs are valid create new user
       const hashedPassword = bcrypt.hashSync(password, saltRounds);
       const newUser = await db.user.create({
         firstName,
